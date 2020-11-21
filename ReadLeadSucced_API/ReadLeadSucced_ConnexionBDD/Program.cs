cd877e4a1b6aa598fa9e57987a5b6b03779c8421
RobertPastiss <gabrielgiroud@gmail.com>
2020-11-21 17:26:04 +0100

    Commit propre

---
+++ ReadLeadSucced_API/ReadLeadSucced_ConnexionBDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadLeadSucced_ConnexionBDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ReadLeadSucced_API/ReadLeadSucced_Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadLeadSucced_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
