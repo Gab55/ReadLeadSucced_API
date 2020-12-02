aad1a38dd8e04ac08f9b3e8e727bc6cbf4863003
Gab <gabrielgiroud@gmail.com>
2020-12-02 13:15:14 +0100

    Importation menu et création front page création utilisateur

--- ReadLeadSucced_Front/src/app/pages/utilisateur/creation-utilisateur/creation-utilisateur.component.ts
+++ ReadLeadSucced_Front/src/app/pages/utilisateur/creation-utilisateur/creation-utilisateur.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-creation-utilisateur',
   templateUrl: './creation-utilisateur.component.html',
-  styleUrls: ['./creation-utilisateur.component.scss'],
+  styleUrls: ['./creation-utilisateur.component.scss', './../../../app.component.scss'],
 })
 export class CreationUtilisateurComponent implements OnInit {
 
