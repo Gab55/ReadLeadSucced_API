3a39bb081e00c7ed2eb548ed6eae94e60b769a7c
tontonmaster <56395118+tontonmaster@users.noreply.github.com>
2020-12-14 01:49:10 +0100

    gestion-commande-ok

--- ReadLeadSucced_Front/src/app/pages/commande/commande.page.ts
+++ ReadLeadSucced_Front/src/app/pages/commande/commande.page.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Client } from 'src/app/models/Client';
+import { Commande } from 'src/app/models/Commande';
 import { LivrePaniers } from 'src/app/models/Panier';
+import { CommandeWebServiceService } from 'src/app/webServices/Commande/commande-web-service.service';
 import { PanierWebService } from 'src/app/webServices/Panier/panier.service';
 import { UtilisateurWebServiceService } from 'src/app/webServices/Utilisateur/utilisateur-web-service.service';
 
@@ -15,14 +19,50 @@ export class CommandePage implements OnInit {
 
   client$: Observable<Client>;
   idClient: string;
+  dataCommande: any;
+  prixTotalHt?: number;
+  etatCommande: string;
   panierVide: boolean;
   panier$: Observable<LivrePaniers[]>;
-  constructor(private clientWebService: UtilisateurWebServiceService,
-    private pService: PanierWebService) { 
+  form: FormGroup;
+  actionType: string;
+  loading = false;
+  identifiant : any;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+  postId: number;
+  existingClientPost: Commande;
 
-}
+  constructor(private clientWebService: UtilisateurWebServiceService,
+    private commandeService: CommandeWebServiceService,
+    private pService: PanierWebService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router) { 
+      const idParam = 'id';
+      this.actionType = 'Add';
+      this.form = this.formBuilder.group(
+        {
+          postId: 0,
+          codeS: ['', [Validators.required]]
+          
+        }
+      )
+     }
+  
+
 
   ngOnInit() {
+
+    if(this.postId >= 0){
+      this.commandeService.getCommandeID(this.postId)
+        .subscribe(data => (
+          this.existingClientPost = data,
+          this.form.controls[this.dataCommande].setValue(data.dataCommande),
+          this.form.controls[this.etatCommande].setValue(data.etatCommande)
+        ));
+    }
     
     if(localStorage.getItem('id') != 'null') 
     {
@@ -46,6 +86,24 @@ export class CommandePage implements OnInit {
         this.panierVide = p[0] != undefined ? false : true; 
       } 
     ));
+  }
+
+  save() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    if (this.actionType === 'Add') {
+      let commande: Commande = {
+        dataCommande: new Date,
+        etatCommande:'validée' ,
+        idClient: Number(this.idClient)}
+
+      this.commandeService.saveCommande(commande)
+        .subscribe((data) => {
+          this.router.navigate(['/livres']);
+        });
+    }
   }
 
 }
