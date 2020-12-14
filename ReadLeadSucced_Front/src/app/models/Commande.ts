3782d5753cd01fe6b0d3d1ee76e553953b542c18
RobertPastiss <gabrielgiroud@gmail.com>
2020-12-14 13:58:25 +0100

    Corrections

--- ReadLeadSucced_Front/src/app/models/Commande.ts
+++ ReadLeadSucced_Front/src/app/models/Commande.ts
@@ -1,8 +1,8 @@
 export class Commande {
     idCommande?: number;
     dataCommande: Date;
-    prixTotalHt:number;
-    prixTotalTtc:number;
+    prixTotalHt?:number;
+    prixTotalTtc?:number;
     etatCommande:string;
     idClient:number;
 }
